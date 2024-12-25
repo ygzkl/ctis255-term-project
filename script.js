2dc977a287c5aa309fe35605b5fe1fd8bd7c8e02
ygzkl <emirkula41@gmail.com>
2024-12-25 15:10:22 +0300

    Add profile page functionality and fix visibilty issue

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ $(function(){
 
             for(let i = 0; i < profiles.length; i++){
                 $(".profile-list").append(`
-                    <a href="https://www.google.com">
+                    <a class="user-wallet-link" href="#">
                    <div class="profile">
                         
                             <button class="rmv-btn">Remove</button>
@@ -57,10 +57,38 @@ $(function(){
         }
     }
 
-    $(".profile-list").on("click", ".rmv-btn", function(){
+    $(".profile-list").on("click", ".rmv-btn", function(e){
         let index = $(this).parent().index();
+        e.preventDefault();
         profiles.splice(index, 1);
         renderProfiles();
     });
 
+
+    $(".profile-list").on("click", ".user-wallet-link", function(){
+        const name = $(this).data("name");
+        // if (name && profiles[name]) {
+          currentProfile = profiles[name];
+ //         $("#current-profile-name").text(name);
+          $(".profile-page").addClass("hidden");
+          $("#main-content").removeClass("hidden");
+          $(".header-right").removeClass("hidden");
+        // }
+      });
+    
+    $(".header-right button").on("click", function(){
+        $(".profile-page").removeClass("hidden");
+        $("#main-content").addClass("hidden");
+        $(".header-right").addClass("hidden");
+    });
 });
+
+if (localStorage.getItem("profilelist")) {
+    profiles = JSON.parse(localStorage.getItem("profilelist"));
+    console.log(profiles);
+}
+
+
+function saveProfiles() {
+    localStorage.setItem("profilelist", JSON.stringify(profiles));
+}
